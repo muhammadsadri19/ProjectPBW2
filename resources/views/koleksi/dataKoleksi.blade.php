058532686c4dc531f0da4b1732220a0dcdb6a140
muhammadsadry19@gmail.com <muhammadsadry19@gmail.com>
2022-11-11 16:34:43 +0700

    Praktikum 7

--- resources/views/koleksi/dataKoleksi.blade.php
+++ resources/views/koleksi/dataKoleksi.blade.php
@@ -3,10 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
 
-    <!doctype html>
-    <html lang="en">
 
     <head>
         <!-- Required meta tags -->
@@ -20,26 +17,7 @@
     </head>
 
     <body>
-        <div class="container mt-3">
-            <h1 class="text-center mb-3">DATA KOLEKSI</h1>
-            <div class="card">
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <th>ID</th>
-                            <th>NAMA KOLEKSI</th>
-                            <th>JENIS KOLEKSI</th>
-                            <th>JUMLAH KOLEKSI</th>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
 
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
-
-    </html>
 
 </x-app-layout>
